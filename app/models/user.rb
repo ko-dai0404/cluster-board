6e0bd8415c6161cd8c375874a47f5f1235f4caa1
ko-dai0404 <rudo.kenkobastyle.19930404@gmail.com>
2022-05-20 10:21:29 +0900

    新規登録/ログイン/ログアウト機能実装とバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname,     presence: true
+  validates :gender_id,    numericality: { other_than: 1 }
+  validates :introduction, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :gender
+  has_one_attached :image
 end
