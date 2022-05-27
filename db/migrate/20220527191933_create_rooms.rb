95c375bb790b01b56cc4bb31be83824e79264dd3
ko-dai0404 <rudo.kenkobastyle.19930404@gmail.com>
2022-05-27 10:50:19 +0900

    マイグレーションファイル修正

---
+++ db/migrate/20220527191933_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :rooms do |t|
+      t.string     :room_name,   null: false
+      t.integer    :category_id, null: false
+      t.text       :content,     null: false
+      t.references :user,        null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
